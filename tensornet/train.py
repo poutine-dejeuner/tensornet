42967a7d51e97b4fc4abfc4009920d4288a9e3d3
Dom <dominique@invivoai.com>
2020-05-01 16:45:07 -0400

    Minor fixes. Removed duplicated train file

--- tensornet/train.py
+++ tensornet/train.py
@@ -2,11 +2,13 @@ import os
 import torch
 import pytorch_lightning
 from pytorch_lightning import Trainer
-from umps import UMPS
-from dataset import MolDataset
 
-from tensornet import regressor
+from tensornet.regressor import Regressor
+from tensornet.umps import UMPS, MultiUMPS
+from tensornet.dataset import MolDataset
+from tensornet.utils import normalise
 
+import tensornet
 
 if __name__ == "__main__":
 
@@ -16,15 +18,23 @@ if __name__ == "__main__":
         gpus = 1
     else:
         gpus = None
-        
-    model = UMPS(feature_dim = 40, bond_dim = 50, output_dim = 19, tensor_init='eye',
-                input_nn_depth=0, input_nn_out_size=32)
 
-    # filedir = os.path.dirname(os.path.realpath(__file__))
-    # dataset = MolDataset(os.path.join(filedir, 'data/qm9.csv'))
-    filedir = os.path.dirname(os.path.realpath(__file__))
-    dataset = MolDataset(os.path.join(filedir, 'data/qm9_mini.csv'))
+    datapath = os.path.join(os.path.dirname(tensornet.__path__._path[0]), 'data/qm9_mini.csv')
+    print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', datapath)
+    normaliser = normalise(datapath)
+    dataset = MolDataset(datapath, normaliser)
+        
+    model = UMPS(feature_dim = 40, bond_dim = 100, output_dim = 19, tensor_init='eye',
+                input_nn_depth=0, input_nn_out_size=32)
+    # model = MultiUMPS(feature_dim = 40, bond_dim = 20, output_dim = 19, tensor_init='norm',
+    #             input_nn_depth=1, input_nn_out_size=32, output_n_umps=4, output_depth=1)
+    
+    
+    
+    regressor = Regressor(model=model, dataset=dataset, transform=normaliser, lr=1e-4, batch_size=4,
+                validation_split=0.2, random_seed=42, num_workers=1)
+
     trainer = Trainer(gpus=gpus, min_epochs=10, max_epochs=20)
-    trainer.fit(model)
+    trainer.fit(regressor)
 
 
