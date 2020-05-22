6189ee838e3bc661a1dde3b65f726e57ae112105
Dom <dominique@invivoai.com>
2020-05-22 14:17:18 -0400

    Added comments to the batch_matmul

--- tensornet/test_batch_matmul.py
+++ tensornet/test_batch_matmul.py
@@ -37,26 +37,29 @@ def chain_matmul_square(As):
 
 
 if __name__ == "__main__":
-    
-    batch = 8
-    num_A = 16
-    size_A = 1000
-    As = torch.randn(num_A, batch, size_A, size_A)
 
+    # Initialize chain of tensors for matmul
+    batch = 4
+    chain_length = 64
+    size_A = 1000
+    As = torch.randn(chain_length, batch, size_A, size_A)
+
+    # Compute using `chain_matmul_square`
     start_time = time.time()
     As_comp = chain_matmul_square(As)
     exec_time = time.time() - start_time
     print('chain_matmul_square time: ', exec_time)
 
-    As_comp2 = As[0:1]
+    # Compute by looping the chain in a standard way
     start_time = time.time()
+    As_comp2 = As[0:1]
     for ii in range(1, As.shape[0]):
         As_comp2 = torch.matmul(As_comp2, As[ii])
     As_comp2 = As_comp2.squeeze(0)
     exec_time = time.time() - start_time
     print('matmul loop time: ', exec_time)
 
-
+    # Make sure that the results from both methods are similar
     diff = (As_comp - As_comp2).abs()
     is_near_eq = torch.all((diff / As_comp2.abs().mean()) < 1e-5)
     print(is_near_eq)
